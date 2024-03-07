ac6d15ca5c2da66ebad5d58025f63fa51189d7b9
Zemorath <trentward10@gmail.com>
2024-03-06 21:13:54 -0600

    Added relationships and serialized rules

--- server/models.py
+++ server/models.py
@@ -6,40 +6,58 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users'
 
+    serialize_rules = ('-subscriptions.user', '-_password_hash',)
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
     name = db.Column(db.String)
     age = db.Column(db.Integer)
     email = db.Column(db.String)
     phone_number = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
+    subscriptions = db.relationship('Subscription', back_popultes='user')
+
     def __repr__(self):
         return f'{self.id}: User {self.username} created.'
+    
+
     
 class Subscription(db.Model):
     __tablename__= 'subscriptions'
 
+    serialize_rules = ('-user.subscriptions', '-provider.subscriptions',)
+
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String)
     sub_price = db.Column(db.Float)
-    service_price = db.Column(db.Float)
+    provider_price = db.Column(db.Float)
     description = db.Column(db.String)
     status = db.Column(db.Boolean)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
-    user_id = db.Column(db.Integer)
-    service_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates='subscriptions')
+
+    provider_id = db.Column(db.Integer, db.ForeignKey('providers.id'))
+    provider = db.relationship('Provider', back_populates='subscriptions')
 
     def __repr__(self):
         return f'Subscription for {self.type} created.'
+    
+
 
 class Provider(db.Model):
     __tablename__ = 'providers'
 
+    serialize_rules = ('-subscriptions.provider',)
+
     id = db.Column(db.Integer, primary_key=True)
     company = db.Column(db.String)
     location = db.Column(db.String)
+
+    subscriptions = db.relationship('Subscription', back_popultes='provider')
 
     def __repr__(self):
         return f'Provder {self.company} located in {self.location} added.'
