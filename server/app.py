2fa2d526c68e2790eadb693b45b9933fdb018f65
Zemorath <trentward10@gmail.com>
2024-03-07 20:17:34 -0600

    Get and post methods added to subscriptions_all resource

--- server/app.py
+++ server/app.py
@@ -65,6 +65,20 @@ class Subscriptions_All(Resource):
     def get(self):
         subscriptions = [sub.to_dict() for sub in Subscription.query.all()]
         return make_response(jsonify(subscriptions), 200)
+    
+    def post(self):
+        data = request.get_json()
+        new_sub = Subscription(
+            type=data.get('type'),
+            sub_price=data.get('sub_price'),
+            provider_price=data.get('provider_price'),
+            description=data.get('description'),
+            status=data.get('status'),
+        )
+        db.session.add(new_sub)
+        db.session.commit()
+
+        return new_sub.to_dict(), 201
 
 class Providers(Resource):
     "Providers will go here"
