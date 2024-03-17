39ebd8ba0cafdca3d2b04e205692d20aae66d581
Zemorath <trentward10@gmail.com>
2024-03-17 17:56:42 -0500

    Form initially created with username and password fields

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,7 +1,30 @@
 import React, { useState } from "react";
 
 function LoginForm({ onLogin }) {
-    //login component here
+    return (
+        <form onSubmit={handleSubmit}>
+            <FormField>
+                <Label htmlFor='username'>Username</Label>
+                <Input
+                    type="text"
+                    id='username'    
+                    autoComplete='off'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </FormField>
+            <FormField>
+                <Label htmlFor='password'>Password</Label>
+                <Input
+                    type='password'
+                    id='password'
+                    autoComplete='current-password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </FormField>
+        </form>
+    )
 }
 
 export default LoginForm
