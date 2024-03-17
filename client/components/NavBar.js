f492c8ddeb55a0d3a45446e5f036c60d4fdbac45
Zemorath <trentward10@gmail.com>
2024-03-17 17:46:16 -0500

    Simple styling for navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../../styles/NavBar.css';
+
 
 function NavBar({ user, setUser}) {
     return (
-        <nav>
+        <nav className='navbar'>
             <ul>
                 <li>
-                    <Link to='/'>Home</Link>
+                    <Link to='/' className="nav-link">Home</Link>
                 </li>
                 <li>
-                    <Link to='/services'>Services</Link>
+                    <Link to='/services' className="nav-link">Services</Link>
                 </li>
                 <li>
-                    <Link to='/account'>Account</Link>
+                    <Link to='/account' className="nav-link">Account</Link>
                 </li>
             </ul>
         </nav>
