2e44bf654bec7658c7e2edb118f8e3008576f45f
Zemorath <trentward10@gmail.com>
2024-03-17 17:35:34 -0500

    Initial setup of navbar

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -2,7 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function NavBar({ user, setUser}) {
-    //set navBar here
-}
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/services'>Services</Link>
+                </li>
+                <li>
+                    <Link to='/account'>Account</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
 export default NavBar
