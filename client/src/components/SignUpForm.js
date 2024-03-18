fb57e3787c7ecb9d40552bc8a46a1ad2f53ee4aa
Zemorath <trentward10@gmail.com>
2024-03-18 18:21:00 -0500

    Implimenting Formik forms in signupform

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,7 +1,58 @@
 import React, { useState } from "react";
+import { Formik } from 'formik';
 
-function SignUpForm({ onSignup }) {
-    //signupform component goes here
+function SignUpForm({ onLogin }) {
+    // const [username, setUsername] = useState("");
+    // const [password, setPassword] = useState("");
+    // const [firstName, setFirstName] = useState("")
+    // const [lastName, setlastName] = useState("")
+    // const [age, setAge] = useState(0)
+    // const [email, setEmail] = useState("")
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrors([]);
+        setIsLoading(true);
+        fetch("/usersignup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                password,
+                password_confirmation: passowrdConfirmation,
+                first_name,
+                last_name,
+                age,
+                email,
+            }),
+        }).then((r) => {
+            setIsLoading(false);
+            if (r.ok) {
+                r.json().then((user) => onLogin(user));
+            } else {
+                r.json().then((err) => setErrors(err.errors));
+            }
+        });
+    }
+
+    return (
+        <div>
+            <Formik
+                initialValues={{ username: '', password: '', first_name: '', last_name: '', age: 0, email: ''}}
+                validate={values => {
+                    const errors = {};
+                    if (!values.username) {
+                        errors.username = 'Required';
+                    }
+                }}
+            >
+
+            </Formik>
+        </div>
+    )
 }
 
 export default SignUpForm
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,7 +3,56 @@ import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignUpForm";
 
 function Login({ onLogin }) {
-    //Login page here
+    const [showLogin, setShowLogin] = useState(true);
+
+    return (
+        <Wrapper>
+            <Logo>Travium</Logo>
+            {showLogin ? (
+                <>
+                    <LoginForm onLogin={onLogin} />
+                    <Divider />
+                    <p>
+                        Don't have an account?
+                        <Button color="secondary" onClick={() => setShowLogin(false)}>
+                            Sign Up
+                        </Button>
+                    </p>
+                </>
+            ) : (
+                <>
+                    <SignUpForm onLogin={onLogin} />
+                    <Divider />
+                    <p>
+                        Already have an account?
+                        <Button color="secondary" onClick={() => setShowLogin(true)}>
+                            Log In
+                        </Button>
+                    </p>
+                </>
+            )}
+        </Wrapper>
+    );
 }
+
+const Logo = styled.h1`
+    font-family: "Courier", monospace;
+    font-size: 3rem;
+    color: black;
+    margin: 8px 0 16px;
+`;
+
+const Wrapper = styled.section`
+    max-width: 500px;
+    margin: 40px auto;
+    padding: 16px;
+`;
+
+const Divider = styled.hr`
+    border: none;
+    border-bottom: 1px solid #ccc;
+    margin: 16px 0;
+`;
+
 
 export default Login
