e42269dd6083a6eddf9ce83defd77ad72d297dcd
Zemorath <trentward10@gmail.com>
2024-03-18 12:34:26 -0500

    fetch created for loginform

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -7,6 +7,25 @@ function LoginForm({ onLogin }) {
     const [errors, setErrors] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsLoading(true);
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username, password}),
+        }).then((r) => {
+            setIsLoading(false);
+            if (r.ok) {
+                r.json().then((user) => onLogin(user));
+            } else {
+                r.json().then((err) => setErrors(err.errors));
+            }
+        });
+    }
+
     return (
         <form onSubmit={handleSubmit}>
             <FormField>
