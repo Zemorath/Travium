22a69cd440d2ec8b298afff957eb06272e1c33e7
Zemorath <trentward10@gmail.com>
2024-06-17 17:05:04 -0500

    Initial deployment successful

--- server/models.py
+++ server/models.py
@@ -58,6 +58,28 @@ class Employee(db.Model, SerializerMixin):
     _password_hash = db.Column(db.String)
     email = db.Column(db.String)
 
+    @validates('username')
+    def validate_username(self, key, _name):
+        if _name != '' and User.query.filter_by(username=_name).first() is None:
+            return _name
+        else:
+            raise ValueError("Username is invalid")
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+
 
     def __repr__(self):
         return f'{self.id}: Employee: {self.username}'
