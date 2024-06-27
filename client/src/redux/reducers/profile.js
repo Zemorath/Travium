68b6326815ee8676a65a03555bd9543becc2651b
Zemorath <trentward10@gmail.com>
2024-06-27 17:34:44 -0500

    small changes

--- client/src/redux/reducers/profile.js
+++ client/src/redux/reducers/profile.js
@@ -1,46 +1,46 @@
-// import { Types } from '../actions/actionTypes'
+import { Types } from '../actions/actionTypes'
 
-// const initialState = {
-//     profile: {
-//         firstName: '',
-//         lastName: '',
-//         email: '',
-//         age: '',
-//         username: '',
-//         password: ''
-//     },
-//     formSubmitted: false
-// }
+const initialState = {
+    profile: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        age: '',
+        username: '',
+        password: ''
+    },
+    formSubmitted: false
+}
 
-// const reducer = (state = initialState, action) => {
-//     switch (action.type) {
-//         case Types.LOGIN:
-//         console.log('login', action.payload.user)
-//             return {
-//                 ...state,
-//                 profile: action.payload.user,
-//                 formSubmitted: false
-//             }
-//         case Types.ADD_USER:
-//             return {
-//                 ...state,
-//                 profile: action.payload.user,
-//                 formSubmitted: false
-//             }
-//         case Types.UPDATE_USER:
-//             return {
-//                 ...state,
-//                 profile: action.payload.user,
-//                 formSubmitted: false
-//             }
-//         case Types.FORM_SUBMITION_STATUS:
-//             return {
-//                 ...state,
-//                 formSubmitted: action.payload.status
-//             }
-//         default:
-//             return state;
-//     }
-// }
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case Types.LOGIN:
+        console.log('login', action.payload.user)
+            return {
+                ...state,
+                profile: action.payload.user,
+                formSubmitted: false
+            }
+        case Types.ADD_USER:
+            return {
+                ...state,
+                profile: action.payload.user,
+                formSubmitted: false
+            }
+        case Types.UPDATE_USER:
+            return {
+                ...state,
+                profile: action.payload.user,
+                formSubmitted: false
+            }
+        case Types.FORM_SUBMITION_STATUS:
+            return {
+                ...state,
+                formSubmitted: action.payload.status
+            }
+        default:
+            return state;
+    }
+}
 
-// export default reducer
+export default reducer
