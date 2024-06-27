c6ea70e2974d7f15059de8ff9bf1b0bda3a49296
Zemorath <trentward10@gmail.com>
2024-06-27 16:30:02 -0500

    small changes

--- client/src/redux/actions/actionTypes.js
+++ client/src/redux/actions/actionTypes.js
@@ -1,6 +1,6 @@
-export const Types = {
-    LOGIN: 'LOGIN',
-    ADD_USER: 'ADD_USER',
-    UPDATE_USER: 'UPDATE_USER',
-    FORM_SUBMITION_STATUS: 'FORM_SUBMITION_STATUS'
-}
+// export const Types = {
+//     LOGIN: 'LOGIN',
+//     ADD_USER: 'ADD_USER',
+//     UPDATE_USER: 'UPDATE_USER',
+//     FORM_SUBMITION_STATUS: 'FORM_SUBMITION_STATUS'
+// }
